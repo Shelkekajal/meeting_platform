d2e6de5a6cc74f6ffdaa06b08c4883b3d1d1f0eb
Shelkekajal <shelkekajal807@gmail.com>
2026-04-13 20:43:48 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ app.use("/api/v1/users", userRoutes);
 const start = async () => {
     app.set("mongo_user")
    
-
+ const connectionDb = await mongoose.connect("mongodb://kajalshelke9889_db_user:<<PASSWAOD")
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
         console.log("LISTENIN ON PORT 8000")
